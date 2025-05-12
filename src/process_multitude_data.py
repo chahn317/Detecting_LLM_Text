bc4da9d4bd2d2a589cd8ad4d137d89ce7ff7ef9d
Jolie Zhou <joliez@uw.edu>
2025-05-12 14:30:18 -0700

    re-split multitude data

--- src/process_multitude_data.py
+++ src/process_multitude_data.py
@@ -12,6 +12,7 @@ from sklearn.model_selection import train_test_split
 TRAIN_SIZE = 150
 VALID_SIZE = 150
 TEST_SIZE = 200
+SEED = 42
 
 # Open MULTITuDE csv
 output_dir = 'dataset/processed_multitude'
@@ -26,58 +27,96 @@ os.makedirs(f"{output_dir}/labels", exist_ok=True)
 os.makedirs(f"{output_dir}/test_data", exist_ok=True)
 os.makedirs(f"{output_dir}/train_valid_data", exist_ok=True)
 
-# Process test split first
-test_split = data[data['split'] == 'test']
-
+# Combine all their data and re-split it
 # Group by language
-grouped_by_language = test_split.groupby('language')
+grouped_by_language = data.groupby('language')
 
 for language, lang_df in grouped_by_language:
-    # Get human data for this language
-    human_df = lang_df[lang_df['multi_label'] == 'human']
+    print("Processing", language)
+    # # First get human samples for this language
+    human_df = lang_df[lang_df['result'] == 0]
+    llm_df = lang_df[lang_df['result'] == 1]
 
     # Use some of the data for a separate general test split
-    if language in ["en", "es", "ru"]:
-        lang_df, test_df = train_test_split(
-            lang_df, 
-            test_size=0.2, 
-            random_state=42, 
-            stratify=lang_df["result"]
-        )
-        human_df, test_human_df = train_test_split(
-            human_df, 
-            test_size=0.2, 
-            random_state=42, 
-            stratify=human_df["result"]
-        )
+    human_df, test_hdf = train_test_split(
+        human_df, 
+        test_size=TEST_SIZE // 2, 
+        random_state=SEED,
+        shuffle=True
+    )
+    llm_df, test_ldf = train_test_split(
+        llm_df, 
+        test_size=TEST_SIZE // 2, 
+        random_state=SEED,
+        shuffle=True
+    )
+    # Use some of the data for a valid split
+    human_df, valid_hdf = train_test_split(
+        human_df, 
+        test_size=VALID_SIZE // 2, 
+        random_state=SEED, 
+        shuffle=True
+    )
+    llm_df, valid_ldf = train_test_split(
+        llm_df, 
+        test_size=VALID_SIZE // 2, 
+        random_state=SEED,
+        shuffle=True
+    )
+    # Use some of the data for a train split
+    human_df, train_hdf = train_test_split(
+        human_df, 
+        test_size=TRAIN_SIZE // 2, 
+        random_state=SEED, 
+        shuffle=True
+    )
+    llm_df, train_ldf = train_test_split(
+        llm_df, 
+        test_size=TRAIN_SIZE // 2, 
+        random_state=SEED, 
+        shuffle=True
+    )
 
-        # Restrict size to 100 of each
-        test_df = test_df.sample(n=(TEST_SIZE // 2), random_state=42)
-        test_human_df = test_human_df.sample(n=min(TEST_SIZE // 2, len(test_human_df)), random_state=42)
-        test_df = pd.concat([test_df, test_human_df])
+    # Format as list of dicts
+    test_df = pd.concat([test_hdf, test_ldf]).sample(frac=1, random_state=SEED).reset_index(drop=True)
+    valid_df = pd.concat([valid_hdf, valid_ldf]).sample(frac=1, random_state=SEED).reset_index(drop=True)
+    train_df = pd.concat([train_hdf, train_ldf]).sample(frac=1, random_state=SEED).reset_index(drop=True)
 
-        # Format as list of dicts
-        test_data = test_df[["text", "result"]].to_dict(orient="records")
-        with open(f"{output_dir}/test_data/{language}_test.json", 'w', encoding='utf-8') as f:
-            json.dump(test_data, f, ensure_ascii=False, indent=4)
+    test_data = test_df[["text", "result"]].to_dict(orient="records")  
+    valid_data = valid_df[["text", "result"]].to_dict(orient="records")  
+    train_data = train_df[["text", "result"]].to_dict(orient="records")  
 
-        # Extract just the labels and save as torch tensors
-        test_labels = torch.tensor(test_df["result"].astype(int).tolist())
-        torch.save(test_labels, f"{output_dir}/labels/{language}_test.pt")
+    # Extract just the labels and save as torch tensors
+    test_labels = torch.tensor(test_df["result"].astype(int).tolist())
+    valid_labels = torch.tensor(valid_df["result"].astype(int).tolist())
+    train_labels = torch.tensor(train_df["result"].astype(int).tolist())
+
+    torch.save(train_labels, f"{output_dir}/labels/{language}_train.pt")
+    torch.save(valid_labels, f"{output_dir}/labels/{language}_val.pt")
+    torch.save(test_labels, f"{output_dir}/labels/{language}_test.pt")
+
+    with open(f"{output_dir}/train_valid_data/{language}_train.json", 'w', encoding='utf-8') as output_file:
+        json.dump(train_data, output_file, ensure_ascii=False, indent=4)
+    with open(f"{output_dir}/train_valid_data/{language}_val.json", 'w', encoding='utf-8') as output_file:
+        json.dump(valid_data, output_file, ensure_ascii=False, indent=4)
+    with open(f"{output_dir}/test_data/{language}_test.json", 'w', encoding='utf-8') as output_file:
+        json.dump(test_data, output_file, ensure_ascii=False, indent=4)
+
 
     # Group the remaining data by multi_label to get test sets
     grouped_by_label = lang_df.groupby('multi_label')
-
     for label, label_df in grouped_by_label:
         # Skip this as we add all the human samples into the other test sets.
         if label == 'human':
             continue
+        
 
-        # Restrict size to 100 of each
-        test_df = label_df.sample(n=(TEST_SIZE // 2), random_state=42)
-        test_human_df = human_df.sample(n=min((TEST_SIZE // 2), len(human_df)), random_state=42)
-        # Combine LLM with human text samples
-        test_df = pd.concat([test_df, test_human_df])
+        # Restrict test set size
+        test_size = min(TEST_SIZE, len(human_df) * 2, len(label_df) * 2)
+        print("Eval set", label, "n =", test_size)
+        test_hdf = human_df.sample(n=test_size // 2, random_state=SEED).reset_index(drop=True)
+        test_ldf = label_df.sample(n=test_size // 2, random_state=SEED).reset_index(drop=True)
+        test_df = pd.concat([test_hdf, test_ldf]).sample(frac=1, random_state=SEED).reset_index(drop=True)
 
         # Format as list of dicts
         test_data = test_df[["text", "result"]].to_dict(orient="records")  
@@ -87,39 +126,3 @@ for language, lang_df in grouped_by_language:
         # Extract just the labels and save as torch tensors
         test_labels = torch.tensor(test_df["result"].astype(int).tolist())
         torch.save(test_labels, f"{output_dir}/labels/{language}_{label}.pt")
-
-
-# Process train split
-train_split = data[data['split'] == 'train']
-
-# Group by language
-grouped_by_language = train_split.groupby('language')
-
-for language, language_group in grouped_by_language:
-    # Split train into train/val (90/10 of the train set)
-    train_df, val_df = train_test_split(
-        language_group, 
-        test_size=0.1, 
-        random_state=42, 
-        stratify=language_group["result"]
-    )
-
-    # Restrict size to 150 of each
-    train_df = train_df.sample(n=TRAIN_SIZE, random_state=42)
-    val_df = val_df.sample(n=VALID_SIZE, random_state=42)
-
-    # Output as list of dicts
-    train_data = train_df[["text", "result"]].to_dict(orient="records")
-    val_data = val_df[["text", "result"]].to_dict(orient="records")
-    
-    with open(f"{output_dir}/train_valid_data/{language}_train.json", 'w', encoding='utf-8') as f:
-        json.dump(train_data, f, ensure_ascii=False, indent=4)
-    with open(f"{output_dir}/train_valid_data/{language}_val.json", 'w', encoding='utf-8') as f:
-        json.dump(val_data, f, ensure_ascii=False, indent=4)
-
-    # Extract just the labels and save as torch tensors
-    train_labels = torch.tensor(train_df["result"].astype(int).tolist())
-    val_labels = torch.tensor(val_df["result"].astype(int).tolist())
-
-    torch.save(train_labels, f"{output_dir}/labels/{language}_train.pt")
-    torch.save(val_labels, f"{output_dir}/labels/{language}_val.pt")
